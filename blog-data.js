364c37ec83eefddcf4ca8ed79e633660323543c4
24mjihad <jihadu2006@gmail.com>
2026-06-24 02:49:07 -0400

    Update blog: Notes on joy

--- blog-data.js
+++ blog-data.js
@@ -11,13 +11,25 @@
 const POSTS = [
 
   {
-    id: "new-post",
+    id: "notes-on-joy",
     date: "2026-06-24",
-    title: "Untitled post",
+    title: "Notes on joy",
     content: `
-## Introduction
+## My summer
 
-Start writing here.
+This summer, I have embraced the joys in my life, from friends to food. I am spending my days in leisure. I have taken up sailing—an expensive endeavor, but one that felt right given the opportunity to create lore and to spend time with friends.
+
+After all, what is the point of being rich if you cannot enjoy it?
+
+I have had the opportunity to chat with some very successful people in the last few weeks leading into summer. There was one question I always made sure to ask before parting ways:
+> "What is one thing you wish you could have right now, that you cannot get?"
+
+Nearly everyone answered with something along the lines of,
+> "I wish I could have more time to enjoy the life that I worked so hard to build."
+
+That is when it hit me: somewhere along the way from studying for the SAT to psetting at Harvard, I lost the plot. That plot being joy. The goal is joy. The reason for success and money is to gain joy, but if you can reach out right now and grab it, why not just do it?
+
+I am doing everything I can to ensure that everyday this summer is filled with joy. I never turn down an opportunity to spend time with people I like.
     `
   },
 
