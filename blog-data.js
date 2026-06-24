620ea294034be46e7138721fccaea8fe855ca961
24mjihad <jihadu2006@gmail.com>
2026-06-24 01:53:34 -0400

    Update blog: Notes on the Industry Job Search

--- blog-data.js
+++ blog-data.js
@@ -11,17 +11,6 @@
 const POSTS = [
 
   {
-    id: "test",
-    date: "2026-06-24",
-    title: "Test",
-    content: `
-## Introduction
-
-Start writing here.
-    `
-  },
-
-  {
     id: "notes-on-industry-job-search",
     date: "2026-06-20",
     title: "Notes on the Industry Job Search",
